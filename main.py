ea6811dd4b48a803ee7ffcc9200ebd205644798a
Devin Greene <devin@greene.cz>
2020-06-25 19:36:35 -0400

    Used subplots for movie and labels

--- main.py
+++ main.py
@@ -43,19 +43,21 @@ plt.rcParams.update({'font.family':'monospace'})
 
 gen = Gen(mu,initial,fitness,recom,exposure_rate,migration_rate)
 
-fig = plt.figure(figsize=(8,8))
-ax = fig.add_axes([0,0,1,1],animated=True)
-img = ax.imshow(initial,
+fig = plt.figure(figsize=(7,6),frameon=False)
+ax_main = fig.add_subplot(121,position = [0,0,6/7,1],frameon=False,yticks=[])
+ax_lab = fig.add_subplot(122,position = [6/7,0,1/7,1],frameon=False,yticks=[])
+img = ax_main.imshow(initial,
         vmin=0,
         vmax=vmax,
-        cmap = "YlOrRd")
+        cmap = "YlOrRd",
+        aspect='auto')
 
 lex = num2lex(list(range(len(fitness))))
 sm = mpl.cm.ScalarMappable(cmap=img.get_cmap())
 sm.set_clim(0,vmax)
 
 for i,l in enumerate(lex):
-    fig.text(0.95,0.95 - 0.035*i,"{:0{:d}b}".format(l,nloci),
+    ax_lab.text(1/2,0.95 - 0.035*i,"{:0{:d}b}".format(l,nloci),
             ha="center",va="center",
             backgroundcolor=sm.to_rgba(l))
 
